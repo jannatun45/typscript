3825f6ec2d285ee624be6f64dc332c0a921f1807
unknown <rulsuyeee@gmail.com>
2025-02-12 09:17:10 +0700

    stylis compnen aside

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,8 @@ export default function App() {
   console.log("routes => ", routes)
   return (
       <div className="flex">
-        <aside className="fixed w-60 inset-y-0 flx-wrap  bg-gray-300">
-          <div className="p-4 bg-gray-800">
-            <a href="http://localhost:5173/">
-              <h2 className="text-xl text-gray-100">Aside</h2>
-            </a>
-          </div>
-          <div className='h-full mt-1'>
+        <aside className="fixed w-60 inset-y-0 bg-gray-300">
+          <div className='h-full flex items-center'>
             <ul className='py-2'>
               <li className='px-3'><a href="/home">home</a></li>
               <li className='px-3'><a href="/about">about</a></li>
